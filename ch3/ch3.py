49124ee5599d0ce57f2a8274cb1bcdc5d36a64cd
Christopher Manna <cmanna285@gmail.com>
2024-03-26 16:48:18 -0700

    "pop" and "pop at" stack with lists

--- ch3/ch3.py
+++ ch3/ch3.py
@@ -147,3 +147,127 @@ class Queue:
 
     def is_empty(self):
         return self.first is None
+
+
+print("QUESTION 3.3")
+class SetOfStacks:
+    #  start a new stack when the previous stack exceeds some threshold
+    def __init__(self):
+        self.top = None
+        self.THRESHOLD = 10
+        self.capacity = 0
+        self.previous_stack = None
+        self.stack_count = 0
+
+    def pop(self):
+        sub_stack = self
+        
+        while sub_stack.previous_stack != None:
+            sub_stack = sub_stack.previous_stack
+        
+        if sub_stack.top is None:
+            if sub_stack.capacity == 0 and sub_stack.previous_stack != None: 
+                sub_stack = sub_stack.previous_stack
+                item = sub_stack.top.data
+                sub_stack.top = sub_stack.top.next
+                sub_stack.capacity -= 1
+                # return item
+                sub_stack = None
+            else:
+                raise EmptyStackException("Stack is empty")
+        else:
+            item = sub_stack.top.data
+            sub_stack.top = sub_stack.top.next
+            sub_stack.capacity -= 1
+        return item
+
+
+    def push(self, item):
+        t = StackNode(item)
+        t.next = self.top
+        # print(f"item: {item}, self.top: {self.top}")
+
+        # met and exceeds the stack threshold
+        if self.capacity == self.THRESHOLD: 
+            # print(f'inside conditional: item: {item}, self.top: {self.top.data}')
+            
+            new_stack = SetOfStacks()
+            new_stack.previous_stack = self
+            new_stack.stack_count = 1 + self.stack_count
+            self = new_stack
+
+        self.capacity += 1
+        self.top = t
+        # print(f"1 self.stack_count: {self.stack_count}; self.top.data: {self.top.data}")
+
+    def peek(self):
+        if self.top is None:
+            raise EmptyStackException("Stack is empty")
+        return self.top.data
+
+    def is_empty(self):
+        return self.top is None
+    
+    def __str__(self) -> str:
+        s = ""
+        if self.top is None: 
+            return ""
+        
+        new_stack  = SetOfStacks()
+        
+        while self.top != None:
+            new_stack.push(self.pop())
+        
+        while new_stack.top != None: 
+            element = new_stack.pop()
+            s += str(element) + ", "
+            self.push(element)
+        return s
+
+class SetOfStacksLists:
+    def __init__(self):
+        self.lists = [list()]
+        self.top = None
+        self.THRESHOLD = 10
+
+    def push(self, item):
+        if len(self.lists) == 0:
+            self.lists.append([])
+        
+        list_index = len(self.lists) - 1
+        if len(self.lists[list_index]) >= self.THRESHOLD:
+            self.lists.append(list())
+        
+        list_index = len(self.lists) - 1
+        self.lists[list_index].append(item)
+    
+    def pop(self):        
+        # no lists
+        if len(self.lists) == 0 or len(self.lists[0]) == 0:
+            raise EmptyStackException("Stack is empty")
+        
+        list_index = len(self.lists) - 1
+        if len(self.lists[list_index]) == 0:
+            self.lists[list_index].pop()
+            self.pop()
+        else: 
+            item = self.lists[list_index].pop()
+            return item
+    
+    def pop_at(self, list_index):
+        # no lists
+        if len(self.lists) == 0 or len(self.lists[0]) == 0 or list_index > len(self.lists) - 1:
+            raise EmptyStackException("Stack is empty")
+        
+        if len(self.lists[list_index]) == 0:
+            self.lists[list_index].pop(list_index)
+            self.pop_at(list_index)
+        else:
+            item = self.lists[list_index].pop()
+            return item
+    def __str__(self) -> str:
+        s = ""
+        for l in self.lists:
+            s += f"{l}"
+        
+        return s
--- ch3/test_ch3.py
+++ ch3/test_ch3.py
@@ -1,5 +1,5 @@
 import unittest, random
-from ch3 import EmptyStackException, StackNode, MyStack
+from ch3 import EmptyStackException, StackNode, MyStack, SetOfStacks, SetOfStacksLists
 
 class TestStack(unittest.TestCase):
     
@@ -9,10 +9,33 @@ class TestStack(unittest.TestCase):
         random.shuffle(zero_to_ten)
         for element in zero_to_ten:
             self.test_stack.push(element)
+        
+
+        self.new_stack_set = SetOfStacksLists()
+        zero_to_twenty = list(range(20))
+        # random.shuffle(zero_to_twenty)
+        for element in zero_to_twenty:
+            self.new_stack_set.push(element)
 
     def test_min(self):
         self.assertEqual(self.test_stack.min(), 0)
+    
+    def test_set_of_stacks_lists_pop(self):
+        self.assertEqual(self.new_stack_set.pop(), 19)
+        # self.assertEqual(self.new_stack_set.pop(), 18)
+        # self.assertEqual(self.new_stack_set.pop(), 17)
+        # self.assertEqual(self.new_stack_set.pop(), 16)
+    def test_set_of_stacks_lists_pop_at_1(self):
+        self.assertEqual(self.new_stack_set.pop_at(1), 19)
 
+    def test_set_of_stacks_lists_pop_at_0(self):
+        self.assertEqual(self.new_stack_set.pop_at(0), 9)
+        # print(self.new_stack_set)
+        self.assertEqual(self.new_stack_set.pop_at(0), 8)
+        
+    # def test_set_of_stacks_lists_pop_at_0(self):
+    #     self.assertEqual(self.new_stack_set.pop_at(0), 8)
+    
 
 if __name__ == '__main__':
     unittest.main()
