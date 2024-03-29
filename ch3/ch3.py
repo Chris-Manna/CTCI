a2d6d5024ef5696e672749deeb93336b4d87a6e6
Christopher Manna <cmanna285@gmail.com>
2024-03-29 13:34:04 -0400

    start problem 3.5

--- ch3/ch3.py
+++ ch3/ch3.py
@@ -57,6 +57,15 @@ class MyStack:
     def is_empty(self):
         return self.top is None
     
+    def sort_stack(self):
+        # Write a program to sort a stack such that the smallest items are on the top. 
+        # You can use an additional temporary stack, but you may not copy the 
+        # elements into any other data structure (such as an array). The stack 
+        # supports the following operations: push, pop, peek, and is Empty.
+        # Hints:# 15, #32, #43 
+        pass
+        
+    
     def __str__(self) -> str:
         s = ""
         if self.top is None: 
