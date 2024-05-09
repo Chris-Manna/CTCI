183dab786afc733e8c8d4b403fe53daafc3dce5d
Christopher Manna <cmanna285@gmail.com>
2024-05-08 17:13:02 -0700

    create class

--- ch8/ch8.py
+++ ch8/ch8.py
@@ -17,12 +17,12 @@ class Stairs:
 
     # 6
     def triple_step_memo(self, total_steps, memo = [0,1,2,4]):
-        
+
         if total_steps <= 3:
             return memo[total_steps]
 
         return (
-            self.triple_step_memo(memo[total_steps - 3], memo) #self.triple_step_memo(memo[6-3], [0,1,2,4])
+            self.triple_step_memo(memo[total_steps - 3], memo) # self.triple_step_memo(memo[6-3], [0,1,2,4])
             + self.triple_step_memo(memo[total_steps - 2], memo)
             + self.triple_step_memo(memo[total_steps - 1], memo)
         )
--- ch8/test_ch8.py
+++ ch8/test_ch8.py
@@ -1,5 +1,5 @@
 import unittest, random
-from ch8 import triple_step
+from ch8 import Stairs
 
 
 class TestGraphs(unittest.TestCase):
@@ -13,17 +13,18 @@ class TestGraphs(unittest.TestCase):
         A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time.
         Implement a method to count how many possible ways the child can run up the stairs.
         """
+        stairs = Stairs()
         # 0
-        self.assertEqual(triple_step(0), 0)
+        self.assertEqual(stairs.triple_step(0), 0)
         # 1
-        self.assertEqual(triple_step(1), 1)
+        self.assertEqual(stairs.triple_step(1), 1)
         # 1+1,                                 2
-        self.assertEqual(triple_step(2), 2)
+        self.assertEqual(stairs.triple_step(2), 2)
         # 1+1+1,                             1+2,   2+1,                     3
-        self.assertEqual(triple_step(3), 4)
+        self.assertEqual(stairs.triple_step(3), 4)
 
         # paths:   1+1+1+(1),   (1)+1+2, (1)+2+1, 2+1+(1), 2+2, 1+3, 3+1 steps
-        # self.assertEqual(triple_step(4), 7)
+        # self.assertEqual(stairs.triple_step(4), 7)
 
         # paths on staircase with 5 steps
         # 1+1+1+1+1, 
@@ -31,14 +32,14 @@ class TestGraphs(unittest.TestCase):
         # 1+2+2,   2+1+2,     2+2+1
         # 1+1+3, 1+3+1, 3+1+1,
         # 2+3, 3+2
-        print(f"5 steps: {triple_step(5)}")
-        print(f"6 steps: {triple_step(6)}")
-        print(f"7 steps: {triple_step(7)}")
-        print(f"8 steps: {triple_step(8)}")
-        print(f"9 steps: {triple_step(9)}")
-        print(f"10 steps: {triple_step(10)}")
+        print(f"5 steps: {stairs.triple_step(5)}")
+        print(f"6 steps: {stairs.triple_step(6)}")
+        print(f"7 steps: {stairs.triple_step(7)}")
+        print(f"8 steps: {stairs.triple_step(8)}")
+        print(f"9 steps: {stairs.triple_step(9)}")
+        print(f"10 steps: {stairs.triple_step(10)}")
         # for i in (range(11,200)):
-        #     print(f"{i} steps: {triple_step(i)}")
+        #     print(f"{i} steps: {stairs.triple_step(i)}")
 
 
 if __name__ == "__main__":
